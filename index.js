c2ac116b0d7149583c6e786c12c0ad6e0cd333f5
ADE <sikirusulaimon2002@gmail.com>
2025-11-11 19:06:54 +0100

    back to default

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-import dotenv from 'dotenv'
 import express from 'express'
+import dotenv from 'dotenv'
 import './config/db.js'
 import cors from 'cors'
 import { Router } from './routes/routes.js'
 
 const app = express()
 app.use(express.json())
-app.use(cors({
-  origin: ["https://contact-manager-client-rose.vercel.app"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}))
-dotenv.config();
+app.use(cors())
 app.use('/contactmsyt', Router)
+dotenv.config();
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Serv
-    er is running on http://localhost:${PORT}
+    er is running on http://localhost:${PORT} ademOla
     `)
 }) 
