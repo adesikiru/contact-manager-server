8912e2e58f6ebfa5a016b02c63524f57d8f42892
ADE <sikirusulaimon2002@gmail.com>
2025-11-11 16:08:27 +0100

    update server side

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Serv
-    er is running on http://localhost:${PORT} ademOla
+    er is running on http://localhost:${PORT}
     `)
 }) 
