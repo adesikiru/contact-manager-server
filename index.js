02dd9ec17d33c0ee9785332f208d18b1cb3fd51b
ADE <sikirusulaimon2002@gmail.com>
2025-11-11 16:59:38 +0100

    update server side

--- index.js
+++ index.js
@@ -9,16 +9,30 @@ import { Router } from './routes/routes.js';
 const app = express();
 app.use(express.json());
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://contact-manager-client-rose.vercel.app",
+];
+
+
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://contact-manager-client-rose.vercel.app",
-    ],
+    origin: function (origin, callback) {
+      // allow requests with no origin like Postman or server-to-server
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
+
 
 app.options('*', cors()); // optional preflight support
 
